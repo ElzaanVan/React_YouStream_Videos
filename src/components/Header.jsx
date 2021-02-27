4526f9cefde0b27d708308048a61166322f3e2cb
ElzaanVan <75164867+ElzaanVan@users.noreply.github.com>
2021-02-27 10:31:20 +0200

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import GoogleAuth from "./GoogleAuth";
+
+const Header = () => {
+  return (
+    <div id="header" className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        YouStream
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Video streams
+        </Link>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
